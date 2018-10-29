2f1428511b7104272189912c732d252ec2eface6
Jeppe Damborg <jeppe_09@hotmail.dk>
2018-10-29 13:24:07 +0100

    en kommentar

--- HovedOpgave/HovedOpgave/Controllers/HomeController.cs
+++ HovedOpgave/HovedOpgave/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace HovedOpgave.Controllers
             return View();
         }
 
+        // Hej Tolga
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
